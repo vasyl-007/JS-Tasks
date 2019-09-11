e341f7823761e4d0f61316234b14a7b708fbc3e6
metamodern <shabanovbunya@gmail.com>
2019-09-11 20:41:10 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ task1(reusable)/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const myName = 'metamodern';
+
+const DATE = 2000;
+
+const greeeting = (name = 'anonymous') => {
+  console.log('Hello, ' + name);
+};
+
+greeeting('timur');
+greeeting(myName);
+greeeting(DATE);
+
+
+let index = 14;
+
+while (index <= 30) {
+  console.log(index);
+  index += 2;
+}
+
+const range = (start = 0, end = 10) => {
+  if (end <= start) {
+    console.log('Start is less than end, fool)');
+    return;
+  }
+  let index = start;
+  while (index <= end) {
+    console.log(index);
+    index += 2;
+  }
+};
+
+range(20, 40);
+
+
